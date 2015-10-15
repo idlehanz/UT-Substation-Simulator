16ae888edb696cb11fdb4b968d5928f94b81d552
JonL13 <JonLamont13@Gmail.com>
2015-10-15 11:55:36 -0400

    update

---
+++ Simulation of Power Substation/Power Substation Simulation/Assets/Scripts/BreakerProperties.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BreakerProperties : MonoBehaviour {
+
+	//public PowerlineProperties powerline;
+	//powerline = GameObject.GetComponent<PowerlineProperties>();
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Simulation of Power Substation/Power Substation Simulation/Assets/Scripts/PowerlineProperties.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerlineProperties : MonoBehaviour {
+
+	public float powerFrequency = 60f;//hertz, HAS TO BE 60!
+	public float current;
+	public float lineImpedance = 34.5f;
+	public float voltage = 69f;
+
+	// Use this for initialization
+	void Start () {
+		voltage = current * lineImpedance;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		current = voltage / lineImpedance; 
+	}
+}
