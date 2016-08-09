def1612b8cb4e4b0c9033949aad6544b7e84693e
tpoff <tpoff@vols.utk.edu>
2016-08-09 13:03:31 -0400

    Added a basic squirrel model with some animations, set up an initial nav mesh, created a simple script for the squirrel
    made a simple box squirrel with 2 animations, idle and hopping(since
    squirrels kinda hop when they run),
    set up an initial nav mesh but it's far from perfect,
    made a script for the squirrel, all it does is traverse the navmesh
    towards the player.

---
+++ Power Substation Simulation/Assets/Scripts/SquirrelMovement.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+class SquirrelMovement:MonoBehaviour
+{
+    Transform player;
+    NavMeshAgent navMesh;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+
+        navMesh = GetComponent<NavMeshAgent>();
+    }
+    void Update()
+    {
+        navMesh.SetDestination(player.position);
+
+    }
+}
+
