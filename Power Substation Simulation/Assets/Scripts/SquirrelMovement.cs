f9b35bdef59a88938708d8da0450fd7e3c1189a8
tpoff <tpoff@vols.utk.edu>
2016-08-09 13:31:27 -0400

    Added a basic animation controller for the squirrel.

--- Power Substation Simulation/Assets/Scripts/SquirrelMovement.cs
+++ Power Substation Simulation/Assets/Scripts/SquirrelMovement.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿/*script to control squirrel movement*/
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,16 +9,37 @@ class SquirrelMovement:MonoBehaviour
 {
     Transform player;
     NavMeshAgent navMesh;
+    Animator animator;
+
     void Start()
     {
+        //get the player transform
         player = GameObject.FindGameObjectWithTag("Player").transform;
-
+        //get teh navmesh
         navMesh = GetComponent<NavMeshAgent>();
+        //get the animator
+        animator = GetComponent<Animator>();
     }
     void Update()
     {
-        navMesh.SetDestination(player.position);
+        //get the distance from the player to the squirrel.
+        float distance = Vector3.Distance(player.position, transform.position);
+        if (distance <=8)
+        {
+            //if the player is within distance, set IsMOving to false andset the destination to the current position
+            animator.SetBool("IsMoving", false);
+            navMesh.SetDestination(transform.position);
+        }
+        else
+        {
+
+            //if the player isn't within distance, set ismoving to true and set teh destination to the player position.
+            animator.SetBool("IsMoving", true);
+            navMesh.SetDestination(player.position);
+        }
 
     }
+
+   
 }
 
