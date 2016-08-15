1b5bf627e8342cddbc0e4efdaf909f80850b0cb2
tpoff <tpoff@vols.utk.edu>
2016-08-15 12:54:16 -0400

    Added a script to spawn squirrels

---
+++ Power Substation Simulation/Assets/Scripts/EventTriggerScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EventTriggerScript : MonoBehaviour {
+    public GameObject squirrelGameObject;
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+
+            Instantiate(squirrelGameObject);
+        }
+	   
+	}
+}
