94d21ae35695bdc873c43af83dd107c9ac4b02b2
tpoff <tpoff@vols.utk.edu>
2016-08-15 13:30:05 -0400

    Added a better event mechanism, added a squirrel death script for squirrel despawning

---
+++ Power Substation Simulation/Assets/Scripts/ActorScripts/SquirrelScripts/SquirrelDeathScript.cs
@@ -0,0 +1,35 @@
+﻿/*this script is to moniter the squirrel and take appropriate action when it should....pass on*/
+
+using UnityEngine;
+using System.Collections;
+
+public class SquirrelDeathScript : MonoBehaviour {
+    //TODO
+    //the idea will be to have 2 prefabs, one for an alive squirrel that will run around and one for a dead squirrel that....well...is dead
+    public GameObject electrocutedSquirrel;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        //check if we should kill the squirrel.
+        if (other.gameObject.tag == "Transformer")
+        {
+            Debug.Log("killing squirrel");
+            Destroy(gameObject);
+        }
+
+        //spawn dead squirrel. TODO
+    }
+
+
+
+
+}
--- Power Substation Simulation/Assets/Scripts/EventTriggerScript.cs
+++ Power Substation Simulation/Assets/Scripts/EventTriggerScript.cs
@@ -1,20 +1,59 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿/*This script is responsable for spawning the actors/objects neccissary for certain events to happen in the simulation.*/
 
-public class EventTriggerScript : MonoBehaviour {
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+//delegate to act as a function pointer for events
+delegate void EventFunction();
+//this struct holds a key to activate an event and a function to call when that event is triggered.
+struct Event
+{
+    public Event(KeyCode newActivateKey, EventFunction newEventFunction)
+    {
+        activateKey = newActivateKey;
+        eventFunction = newEventFunction;
+    }
+    public void checkForTrigger()
+    {
+        if (Input.GetKeyDown(activateKey))
+        {
+            eventFunction();
+        }
+    }
+    KeyCode activateKey;
+    EventFunction eventFunction;
+
+}
+
+class EventTriggerScript : MonoBehaviour {
+
+
+    List<Event> events;//list of events
+
+    //a reference to our squirrel prefab game object
     public GameObject squirrelGameObject;
-	// Use this for initialization
+    // Use this for initialization
 	void Start () {
-	    
+        events = new List<Event>();
+
+        //add the squirrel incident event
+        events.Add(new Event(KeyCode.Q, squirrelIncident));
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        if (Input.GetKeyDown(KeyCode.Q))
+        
+        foreach (Event e in events)
         {
-
-            Instantiate(squirrelGameObject);
+            e.checkForTrigger();
         }
 	   
 	}
+
+    //this function is to trigger the squirrel incident.
+    void squirrelIncident()
+    {
+        Instantiate(squirrelGameObject);
+    }
 }
