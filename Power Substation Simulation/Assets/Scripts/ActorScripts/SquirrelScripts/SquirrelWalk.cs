040d095b33536b98cffec081ba2874f51c8a609b
astewa44 <astewa44@utk.edu>
2016-09-25 20:00:47 -0400

    Smoothed Squirrel face, Created Squirrel texture, Created Squirrel Prefab, Created SquirrelWalk script

---
+++ Power Substation Simulation/Assets/Scripts/ActorScripts/SquirrelScripts/SquirrelWalk.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SquirrelWalk : MonoBehaviour {
+
+	public float speed = 1.5f;
+	public float distance = 40f;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (distance > 0) {
+			Vector3 velocity = transform.forward * Time.deltaTime * speed;
+			distance -= velocity.magnitude;
+			transform.position += velocity;
+		}
+	}
+}
