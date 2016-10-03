e9b3ca66932ca60483b0e1ea66f75596956fa255
tpoff <tpoff@vols.utk.edu>
2016-10-03 00:28:34 -0400

    Added ragdoll to squirrel model,
    added a ragdoll to the squirrel model
    set a trigger function for ragdoll in squirrelWalk script, can be moved
    later,
    hooked up animation controller to squirrel, tested all animation and
    transitions and they work.

--- Power Substation Simulation/Assets/Scripts/ActorScripts/SquirrelScripts/SquirrelWalk.cs
+++ Power Substation Simulation/Assets/Scripts/ActorScripts/SquirrelScripts/SquirrelWalk.cs
@@ -5,17 +5,39 @@ public class SquirrelWalk : MonoBehaviour {
 
 	public float speed = 1.5f;
 	public float distance = 40f;
+    Animator animator;
+    public bool ragdollState = false;
 
-	// Use this for initialization
-	void Start () {
-	}
+    // Use this for initialization
+    void Start () {
+        setRagdollState(ragdollState);
+
+        animator = GetComponent<Animator>();
+        animator.SetBool("Walking", false);
+        animator.SetBool("Climbing", false);
+        animator.SetBool("Dead", true);
+        animator.SetBool("Grabbing", false);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		if (distance > 0) {
-			Vector3 velocity = transform.forward * Time.deltaTime * speed;
-			distance -= velocity.magnitude;
-			transform.position += velocity;
-		}
+		
 	}
+
+
+
+
+
+    void setRagdollState(bool newValue)
+    {
+        ragdollState = newValue;
+        //Get an array of components that are of type Rigidbody
+        Rigidbody[] bodies = GetComponentsInChildren<Rigidbody>();
+
+        //For each of the components in the array, treat the component as a Rigidbody and set its isKinematic property
+        foreach (Rigidbody rb in bodies)
+        {
+            rb.isKinematic = !newValue;
+        }
+    }
 }
