b8476e1095e9684e0ae1829943c3b7765549f9ad
tpoff <tpoff@vols.utk.edu>
2016-10-11 12:23:17 -0400

    Added a basic switch, started adding functionality via a script
    I made a basic switch model and a script to rotate the lever arm. I
    started working on a system to pass in 'events' to the lever script that
    woudl be triggered when it is interacted with, however it's proving
    harder than I expected and isn't functional at the moment. Commiting to
    ensure my work is saved.

---
+++ Power Substation Simulation/Assets/Scripts/EventScripts/LeverScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+
+public class LeverScript : MonoBehaviour
+{
+    public UnityEvent onTriggerEvent;
+    public Transform armTransform;
+    float x = 0;
+
+    void Start()
+    {
+        onTriggerEvent.Invoke();
+
+    }
+    void Update()
+    {
+        x = .5f;
+        armTransform.Rotate(new Vector3(0, x, 0));
+
+    }
+
+    public void armUpEvent()
+    {
+        Debug.Log("Arm up");
+    }
+    public void armDownEvent()
+    {
+        Debug.Log("Arm Down");
+    }
+}
---
+++ Power Substation Simulation/Assets/Scripts/EventScripts/SquirrelIncidentEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SquirrelIncidentEvent
+{ 
+    public static void trigger()
+    {
+        Debug.Log("test");
+    }
+}
