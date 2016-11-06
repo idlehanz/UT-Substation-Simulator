a329de645aea8ed6da847fa1722930704fc9b003
tpoff <tpoff@vols.utk.edu>
2016-11-06 01:33:42 -0500

    Reimplemented my changes from earlier
    changes to transformer were removed in a previous push earlier today.

--- Power Substation Simulation/Assets/Scripts/Electrical Component Scripts/TransformerScript.cs
+++ Power Substation Simulation/Assets/Scripts/Electrical Component Scripts/TransformerScript.cs
@@ -16,7 +16,10 @@ class TransformerScript : ElectricalComponentScript
 	public Light lt;
 	public float c;
 
-	public override void uniqueStart()
+
+    protected SquirrelScript squirrel = null;
+
+    public override void uniqueStart()
 	{
 
 	}
@@ -28,97 +31,83 @@ class TransformerScript : ElectricalComponentScript
 	//update the output.
 	public override void updateOutput()
 	{
-		if (damaged == false)
-		{
-			GameObject tmpasadf = GameObject.Find ("LightColorShift");
-			lt = tmpasadf.GetComponent<Light> ();
-			c = 1;
-			output.current = input.current;
-			output.voltage = input.voltage / step;
-			output.frequency = input.frequency;
-			lt.color = Color.red;
+        if(damaged == false)
+        {
+            GameObject tmpasadf = GameObject.Find("LightColorShift");
+            lt = tmpasadf.GetComponent<Light>();
+            c = 1;
+            output.current = input.current;
+            output.voltage = input.voltage / step;
+            output.frequency = input.frequency;
+            lt.color = Color.red;
 
 
-		}
-		else
-		{
+        }
+        else
+        {
 
-			output.current = 0;
-			output.voltage = 0;
-			output.frequency = 0;
-			c = output.voltage;
-			lt.color = Color.green;
-		}
-		//lt.color = Color.Lerp(Color.green, Color.red, c);
-	}
-
+            output.current = 0;
+            output.voltage = 0;
+            output.frequency = 0;
+            c = output.voltage;
+            lt.color = Color.green;
+        }
+    }
 
 
-	public void triggerElectricalDamage()
-	{
-		if (explosion == null)
-		{
-			explosion = GetComponent<AudioSource>();
-			explosion.Play();
-		}
-		damaged = true;
 
-	}
+    public void triggerElectricalDamage()
+    {
+        if (damaged == false)
+        {
+            damaged = true;
+            if (explosion == null)
+            {
+                explosion = GetComponent<AudioSource>();
 
+            }
+            explosion.Play();
+            if (electricalExplosionParticles != null)
+            {
+                electricalExplosionParticles.Play();
+            }
+            Debug.Log("transformer damaged");
+        }
 
-	void OnTriggerEnter(Collider  other)
-	{
-		if (other.gameObject.tag == "squirrel")
-		{
-			if (damaged == false)
-			{
-				damaged = true;
-
-				if (explosion == null)
-				{
-					explosion = GetComponent<AudioSource>();
-					explosion.Play();
-				}
-				if (electricalExplosionParticles != null)
-				{
-					electricalExplosionParticles.Play();
-				}
-				Debug.Log("transformer damaged");
-			}
-		}
-	}
-	void OnTriggerStay(Collider other)
-	{
-
-	}
-	void OnTriggerExit(Collider other)
-	{
-
-	}
-
-
-	public bool isDestroyed()
-	{
-		return damaged;
-	}
+    }
 
 
 
 
-	public override void onInteract(GameObject interactor)
-	{
-		Debug.Log("interacting with transformer");
-		damaged = false;
 
-	}
-	public override void onDisplayInteractionMessage(GameObject interactor)
-	{
-		//draw a box containing relevant information about the transformer.
-		GUI.color = Color.white;
-		GUI.Box(new Rect(20, 20, 200, 55), "Transformer Voltage: " + output.voltage.ToString() + "\n" +
-			"Transformer Frequency: " + output.frequency.ToString() + "\nTransformer Current: " + output.current.ToString());
+    public bool isDestroyed()
+    {
+        return damaged;
+    }
 
 
 
-	}
+
+    public override void onInteract(GameObject interactor)
+    {
+        if (interactor.tag == "squirrel")
+        {
+            triggerElectricalDamage();
+            squirrel = interactor.GetComponent<SquirrelScript>();
+        }
+        else
+            if (squirrel.isPinned() == false)
+            damaged = false;
+
+    }
+    public override void onDisplayInteractionMessage(GameObject interactor)
+    {
+        //draw a box containing relevant information about the transformer.
+        GUI.color = Color.white;
+        GUI.Box(new Rect(20, 20, 200, 55), "Transformer Voltage: " + output.voltage.ToString() + "\n" +
+                "Transformer Frequency: " + output.frequency.ToString() + "\nTransformer Current: " + output.current.ToString());
+
+
+
+    }
 }
