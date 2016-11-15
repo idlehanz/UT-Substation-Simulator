f4d91db99594500d3565c2a329e890963793a06d
tpoff <tpoff@vols.utk.edu>
2016-11-15 10:39:32 -0500

    Fixed a problem with the moveable physics object script,
    found a wierd bug that was introduced somewhere along the line, anytime
    we picked up an object with this script it would position itself at a
    wierd angle relative to the camera. Anyway it's fixed now.

--- Power Substation Simulation/Assets/Scripts/MoveablePhysicObjectScript.cs
+++ Power Substation Simulation/Assets/Scripts/MoveablePhysicObjectScript.cs
@@ -1,49 +1,38 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class MoveablePhysicObjectScript : MonoBehaviour,Interactable {
+public class MoveablePhysicObjectScript : MonoBehaviour, Interactable
+{
 
     GameObject heldBy = null;
     bool held = false;
-    public float horizontalDistance = .5f;
-    public float verticalDistance = 1;
+    public float vel = 1.0f;
     Rigidbody rigidBody;
-	// Use this for initialization
-	void Start () {
-	    rigidBody = GetComponent<Rigidbody>(); ;
+    // Use this for initialization
+    void Start()
+    {
+        rigidBody = GetComponent<Rigidbody>(); ;
     }
-	
-	// Update is called once per frame
-	void Update () {
-	    if (held ==true)
-        {//bad bad bad, but for some reason if I search for the camera in the player it returns null.
-            Transform cameraTransform = Camera.main.transform;
 
-
-
-            rigidBody.detectCollisions = false;
+    // Update is called once per frame
+    void Update()
+    {
+        if (held == true)
+        {
             rigidBody.useGravity = false;
             rigidBody.isKinematic = true;
 
 
+            float horizontalComponent = Input.GetAxisRaw("Horizontal");//should we move forward?
+            float verticleComponent = Input.GetAxisRaw("Vertical");//how about strafing?
 
-            Vector3 horizontalVelocity = Vector3.Cross(cameraTransform.up, cameraTransform.transform.forward).normalized * horizontalDistance;
-            Vector3 verticalVelocity = Vector3.Cross(cameraTransform.up, -cameraTransform.transform.right).normalized * verticalDistance;
-            Vector3 velocity = verticalVelocity + horizontalVelocity;
+            Vector3 horizontalVelocity = Vector3.Cross(transform.up, heldBy.transform.forward).normalized * vel;
+            Vector3 verticalVelocity = Vector3.Cross(transform.up, -heldBy.transform.right).normalized * vel;
+            Vector3 velocity = verticalVelocity;// + horizontalVelocity;
 
-            Vector3 hudPosition = cameraTransform.position + velocity;
-            transform.position = hudPosition;
-
-
-
-
-
-            //transform.Rotate(cameraTransform.rotation.eulerAngles + hudRotation);
-            //transform.rotation.SetEulerAngles(cameraTransform.rotation.eulerAngles + hudRotation);
-            transform.rotation = cameraTransform.rotation;
-            
-            transform.parent = cameraTransform;
-
+            //rigidBody.MovePosition(heldBy.transform.position+ velocity);
+            transform.position = heldBy.transform.position + velocity;
+            transform.rotation = heldBy.transform.rotation;
 
         }
         else
@@ -51,13 +40,14 @@ public class MoveablePhysicObjectScript : MonoBehaviour,Interactable {
             rigidBody.useGravity = true;
             rigidBody.isKinematic = false;
         }
-	}
+    }
 
     public void interact(GameObject interactor)
     {
         if (held == false)
         {
-            heldBy = interactor;
+            Transform t = Camera.main.transform;
+            heldBy = t.gameObject;
             held = true;
         }
         else
