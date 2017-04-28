c9812b724dbca76df58003330e1128185b687d75
Preston Shires <pshires1@vols.utk.edu>
2017-04-28 01:06:06 -0400

    add display message to inform user of their delivery of oil to the lab

--- Power Substation Simulation/Assets/Scripts/ToolScript.cs
+++ Power Substation Simulation/Assets/Scripts/ToolScript.cs
@@ -14,6 +14,8 @@ public class ToolScript : MonoBehaviour, Interactable {
     public GameObject parentObject;
     public Rigidbody rigidBody;
 
+	public bool isOilDelivered = false;
+
     // Use this for initialization
     void Start () {
         rigidBody = GetComponent<Rigidbody>();
@@ -30,10 +32,9 @@ public class ToolScript : MonoBehaviour, Interactable {
 	
 	// Update is called once per frame
 	void Update () {
-	   if (Input.GetKeyDown("u"))
-        {
-            dropTool();
-        }
+		if (Input.GetKeyDown ("u")) {
+			dropTool();
+		}
 	}
 
     void LateUpdate()
@@ -58,9 +59,23 @@ public class ToolScript : MonoBehaviour, Interactable {
         //determines if the object needs to be removed (sent to the lab)
 		if (rigidBody.GetComponent<Renderer>().material.color == Color.black)
         {
-            rigidBody.gameObject.SetActive(false); //deliver it to the lab
+			isOilDelivered = true;
+			Invoke ("displayMsg", 3);
         }
     }
+
+	public void displayMsg() {
+		isOilDelivered = false;
+		rigidBody.gameObject.SetActive (false); // remove bucket of oil from scene
+	}
+
+	public void OnGUI() {
+		if (isOilDelivered == true) {
+			GUI.color = Color.white;
+			GUI.Box (new Rect (20, 20, 360, 30), "You have successfully delivered the oil sample to the lab!");
+		}
+	}
+
     public void pickUpTool(GameObject interactor)
     {
         //bad bad bad, but for some reason if I search for the camera in the player it returns null.
@@ -97,7 +112,7 @@ public class ToolScript : MonoBehaviour, Interactable {
         {
             GUI.color = Color.white;
             GUI.Box(new Rect(20, 20, 200, 55), "Press e to pick up " + toolName);
-		}
+		} 
     }
 
     public void interact(GameObject interactor)
